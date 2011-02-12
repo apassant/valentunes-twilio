c121571d8af6f4cfcde82ae2689ea69a2b6d2e37
Alexandre Passant <alexandre.passant@deri.org>
2011-02-12 17:14:28 -0500

    Initial commit

---
+++ config.dist.py
@@ -0,0 +1,15 @@
+# Twilio REST API version
+API_VERSION = '2010-04-01'
+
+# Twilio AccountSid and AuthToken
+ACCOUNT_SID = 'GET_ONE-FROM_TWILIO'
+ACCOUNT_TOKEN = 'GET_ONE-FROM_TWILIO'
+
+# Outgoing Caller ID previously validated with Twilio
+CALLER_ID = 'USE_YOUR_NUMBER';
+
+# Root folder of your webservice
+ROOT = 'EDIT_THIS'
+
+# Default song
+DEFAULT_SONG = 'EDIT_THIS'
---
+++ valentunes.py
@@ -0,0 +1,43 @@
+import twilio
+import uuid
+import config
+
+## HOW-TO
+## - Copy config.dist.py to config.py and edit parameters
+## - Run valentunes.py
+
+## @@FIXME : Get parameters from the URL
+_to = 'Julie'
+_from = 'Alex'
+_phone = config.CALLER_ID
+
+_voice = twilio.Say.MAN
+_lang = twilio.Say.ENGLISH
+_song = config.DEFAULT_SONG
+
+## Generate twilio message
+r = twilio.Response()
+r.addSay("Hello %s, this is %s. I got a song for you, happy Valentine's day !" %(_to, _from), 
+    voice = _voice, 
+    language = _lang,
+    )
+r.addPlay(_song)
+
+## Save message file
+name = uuid.uuid4()
+f = open("./data/%s" %name, 'w')
+f.write("%s" %r)
+f.close()
+
+## Ring the phone
+account = twilio.Account(config.ACCOUNT_SID, config.ACCOUNT_TOKEN)
+d = {
+    'From' : config.CALLER_ID,
+    'To' : _phone,
+    'Url' : config.ROOT + "data/%s" %name,
+}
+try:
+    print account.request('/%s/Accounts/%s/Calls' %(config.API_VERSION, config.ACCOUNT_SID), 'POST', d)
+except Exception, e:
+    print e
+    print e.read()
