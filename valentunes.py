995263589ca7ce06d7388b59ad442f23375de98f
Alexandre Passant <alexandre.passant@deri.org>
2011-02-12 18:32:06 -0500

    Using web.py

--- valentunes.py
+++ valentunes.py
@@ -1,5 +1,6 @@
 import uuid
-import sys
+import os, sys
+import web
 
 path = os.path.dirname(__file__)
 sys.path.append("%s/" %path)
@@ -7,42 +8,51 @@ sys.path.append("%s/" %path)
 import twilio
 import config
 
-## HOW-TO
-## - Copy config.dist.py to config.py and edit parameters
-## - Run valentunes.py
+urls = (
+    '/.*', 'Caller',
+)
 
-## @@FIXME : Get parameters from the URL
-_to = 'Julie'
-_from = 'Alex'
-_phone = config.CALLER_ID
+class Caller:
+    
+    def call(self):
+        
+        ## @@FIXME : Get parameters from the URL
+        _to = 'Julie'
+        _from = 'Alex'
+        _phone = config.CALLER_ID
 
-_voice = twilio.Say.MAN
-_lang = twilio.Say.ENGLISH
-_song = config.DEFAULT_SONG
+        _voice = twilio.Say.MAN
+        _lang = twilio.Say.ENGLISH
+        _song = config.DEFAULT_SONG
 
-## Generate twilio message
-r = twilio.Response()
-r.addSay("Hello %s, this is %s. I got a song for you, happy Valentine's day !" %(_to, _from), 
-    voice = _voice, 
-    language = _lang,
-    )
-r.addPlay(_song)
+        ## Generate twilio message
+        r = twilio.Response()
+        r.addSay("Hello %s, this is %s. I got a song for you, happy Valentine's day !" %(_to, _from), 
+            voice = _voice, 
+            language = _lang,
+            )
+        r.addPlay(_song)
 
-## Save message file
-name = uuid.uuid4()
-f = open("%s/data/%s" %(path, name), 'w')
-f.write('<?xml version="1.0" encoding="UTF-8" ?>\n %s' %r)
-f.close()
+        ## Save message file
+        name = uuid.uuid4()
+        f = open("%s/data/%s" %(path, name), 'w')
+        f.write('<?xml version="1.0" encoding="UTF-8" ?>\n %s' %r)
+        f.close()
 
-## Ring the phone
-account = twilio.Account(config.ACCOUNT_SID, config.ACCOUNT_TOKEN)
-d = {
-    'From' : config.CALLER_ID,
-    'To' : _phone,
-    'Url' : config.ROOT + "/data/%s" %name,
-}
-try:
-    print account.request('/%s/Accounts/%s/Calls' %(config.API_VERSION, config.ACCOUNT_SID), 'POST', d)
-except Exception, e:
-    print e
-    print e.read()
+        ## Ring the phone
+        account = twilio.Account(config.ACCOUNT_SID, config.ACCOUNT_TOKEN)
+        d = {
+            'From' : config.CALLER_ID,
+            'To' : _phone,
+            'Url' : config.ROOT + "/data/%s" %name,
+        }
+        try:
+            print account.request('/%s/Accounts/%s/Calls' %(config.API_VERSION, config.ACCOUNT_SID), 'POST', d)
+        except Exception, e:
+            print e
+            print e.read()
+
+    def GET(self):
+        return self.call()
+
+application = web.application(urls, globals()).wsgifunc()
