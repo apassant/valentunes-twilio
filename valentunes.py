44da01022345b6a38ff0efc379498ed503de3a54
Alexandre Passant <alexandre.passant@deri.org>
2011-02-12 21:15:36 -0500

    Split library and CGI

--- valentunes.py
+++ valentunes.py
@@ -1,6 +1,5 @@
 import uuid
 import os, sys
-import web
 
 path = os.path.dirname(__file__)
 sys.path.append("%s/" %path)
@@ -8,58 +7,55 @@ sys.path.append("%s/" %path)
 import twilio
 import config
 
-urls = (
-    '/.*', 'Caller',
-)
-
-class Caller:
+class Valentunes:
     
-    def call(self, args):
-        
-        ## Mandatory parameters
-        _to = args._to
-        _from = args._from
-        _phone = args._phone
+    def __init__(self, args):
+        ## Phone number
+        self._phone = args._phone
 
-        ## Custom message
+        ## Message
+        message = "Hello %s, this is %s. Here is a song for you, happy Valentine's day !" %(args._to, args._from)
         if '_msg' in args.keys():
-            _msg = args._msg
-
+            self._msg = "%s %s" %(message, args._msg)
+        else:
+            self._msg = message
+        
         ## Song
         if '_song' in args.keys():
-            _song = args._song
+            self._song = args._song
         else:
-            _song = config.DEFAULT_SONG
+            self._song = config.DEFAULT_SONG
         
         ## Voice
         if '_voice' in args.keys():
             if args._voice == 'woman':
-                _voice = twilio.Say.WOMAN
+                self._voice = twilio.Say.WOMAN
         else:
-            _voice = twilio.Say.MAN
+            self._voice = twilio.Say.MAN
 
         ## Lang
         if '_lang' in args.keys():
             if args._lang == 'es':
-                _lang = twilio.Say.ENGLISH
+                self._lang = twilio.Say.ENGLISH
             if args._lang == 'fr':
-                _lang = twilio.Say.FRENCH
+                self._lang = twilio.Say.FRENCH
             if args._lang == 'de':
-                _lang = twilio.Say.GERMAN
+                self._lang = twilio.Say.GERMAN
         else:
-            _lang = twilio.Say.ENGLISH
-                    
-
+            self._lang = twilio.Say.ENGLISH
+                
+    def call(self):
+        
+        global path
+        
         ## Generate Twilio message
         r = twilio.Response()
-        message = "Hello %s, this is %s. Here is a song for you, happy Valentine's day !" %(_to, _from)
-        if _msg:
-            message = "%s %s" %(message, _msg)
-        r.addSay(message,
-            voice = _voice,
-            language = _lang,
+        r.addSay(
+            self._message,
+            voice = self._voice,
+            language = self._lang,
         )
-        r.addPlay(_song)
+        r.addPlay(self._song)
 
         ## Save message file
         name = uuid.uuid4()
@@ -71,7 +67,7 @@ class Caller:
         account = twilio.Account(config.ACCOUNT_SID, config.ACCOUNT_TOKEN)
         d = {
             'From' : config.CALLER_ID,
-            'To' : _phone,
+            'To' : self._phone,
             'Url' : config.ROOT + "/data/%s" %name,
         }
         try:
@@ -79,17 +75,3 @@ class Caller:
         except Exception, e:
             print e
             print e.read()
-
-    def GET(self):
-        args = web.input()
-        ## Check required parameters
-        if '_to' not in args.keys():
-            return "_to parameter required (name of the person you want to call)"
-        if '_from' not in args.keys():
-            return "_from parameter required (your name)"
-        if '_phone' not in args.keys():
-            return "_phone parameter required (phone number to ring)"
-        ## Go !
-        return self.call(args)
-
-application = web.application(urls, globals()).wsgifunc()
